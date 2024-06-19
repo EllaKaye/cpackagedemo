04e0109e4ed4fda5acdecf521f4a6db8c50fd3db
EllaKaye <ella.kaye@gmail.com>
2024-06-19 16:33:13 +0100

    double test

--- inst/examples/c_rle.R
+++ inst/examples/c_rle.R
@@ -1,5 +1,6 @@
 x <- c(1, 1, 3, 3, 3, 3, 2, 2, 2)
 y <- c(1, 1, 3, 3, 3, 3, 2, 2, 4)
+z <- c(rep(1.2, 3), rep(2.4, 2))
 
 source("./inst/examples/csource.R")  # defines csource
 
@@ -7,3 +8,5 @@ csource("./inst/examples/c_rle.c")
 
 c_rle(x)
 c_rle(y)
+c_rle(z)
+
